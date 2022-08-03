ff9deace31ec5345736606782e378ea33cde00ee
Matej Bizjak <matejb96@gmail.com>
2022-08-03 22:13:00 +0200

    User service

---
+++ user-service/api/v1/src/main/java/com/matejbizjak/user/config/AuthRole.java
@@ -0,0 +1,7 @@
+package com.matejbizjak.user.config;
+
+public class AuthRole {
+
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+}
---
+++ user-service/services/src/main/java/com/matejbizjak/user/config/ConfigProperties.java
@@ -0,0 +1,29 @@
+package com.matejbizjak.user.config;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@ConfigBundle("app-urls")
+public class ConfigProperties {
+
+    private String keycloakUrl;
+    private String keycloakRealm;
+
+    public String getKeycloakUrl() {
+        return keycloakUrl;
+    }
+
+    public void setKeycloakUrl(String keycloakUrl) {
+        this.keycloakUrl = keycloakUrl;
+    }
+
+    public String getKeycloakRealm() {
+        return keycloakRealm;
+    }
+
+    public void setKeycloakRealm(String keycloakRealm) {
+        this.keycloakRealm = keycloakRealm;
+    }
+}
